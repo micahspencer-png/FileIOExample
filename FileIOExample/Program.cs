1b77a9742d0c97f56163c7a8fbffb5288c47129a
micahspencer-png <micahspencer@isu.edu>
2026-02-17 09:18:44 -0700

    Add project files.

---
+++ FileIOExample/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace FileIOExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+            
+            
+            
+            Console.Read();
+        }
+    }
+}
